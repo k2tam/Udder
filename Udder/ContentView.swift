58d08908c08a184de7f9f58570d77e3605bb4678
Tam Bui <buingockhaitam01@gmail.com>
2024-01-28 11:27:07 +0700

    add logo

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("UDDER")
+            
+            Image(systemName: "apple.logo")
         }
         .padding()
     }
