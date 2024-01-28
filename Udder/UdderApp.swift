f6e2d4e8518ad068c08780532665437a2515a22b
Tam Bui <buingockhaitam01@gmail.com>
2024-01-28 11:19:32 +0700

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by k2 tam on 28/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
